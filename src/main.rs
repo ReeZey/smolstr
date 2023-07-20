052de97e407cabe2f80c70e70e9190c0ee164840
ReeZey <johannes@seved.net>
2023-07-20 20:21:14 +0200

    yo

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,34 @@
-use std::{fs::{self, File}, io::{BufWriter, Write}, ops::Index};
+use std::{fs::{self, File}, io::{BufWriter, Write, Read}, ops::Index, time::SystemTime, path::Path};
 use byteorder::{ReadBytesExt, BigEndian};
-use roman::Roman;
-
-mod roman;
 
 fn main() {
-    let chars: &str = " abcdefghijklmnopqrstuvwxyz.!?";
+    let output_dir = Path::new("output");
+    if !output_dir.exists() {
+        fs::create_dir(output_dir).unwrap();
+    }
+    
+
+    let start = SystemTime::now();
+    let chars: &str = " abcdefghijklmnopqrstuvwxyz.!?12";
 
     let mut text: String = String::new();
-    text = "hej alfred".to_owned();
-    //File::open("bible2022.txt").unwrap().read_to_string(&mut text).unwrap();
+    File::open("bible2022.txt").unwrap().read_to_string(&mut text).unwrap();
     
     let encoded_text = compress(chars, &text);
 
-    fs::write("dump.bin", encoded_text).unwrap();
-    fs::write("dump.txt", text.as_bytes()).unwrap();
+    //println!("{:?}", encoded_text);
+    let checksum = crc32fast::hash(&encoded_text);
+    println!("currently writing {}", checksum);
+    println!("{}/{}.smol", output_dir.to_string_lossy(), checksum);
+    fs::write(output_dir.join(format!("{}.smol",checksum)), encoded_text).unwrap();
+    fs::write(output_dir.join(format!("{}.txt",checksum)), text.as_bytes()).unwrap();
 
     //println!("yo");
-    let text: String = decompress("dump.bin", chars);
-    println!("{}", text);
+    //let text: String = 
+    decompress(&output_dir.join(format!("{}.smol",checksum)).to_string_lossy(), chars);
+    //println!("{}", text);
+
+    println!("done, took {:?}", start.elapsed().unwrap());
 }
 
 fn char_to_index(chars: &str, searced_str: char) -> Option<usize> {
@@ -35,107 +45,140 @@ fn compress(chars: &str, text: &String) -> Vec<u8> {
 
     let mut value: u128 = 0;
     let mut counter = 0;
-    //let mut current_number = vec![];
-    for t in text.chars() {
-        /*
-        if t.is_numeric() {
-            current_number.push(t.to_string());
+    let mut current_number = vec![];
+    for mut char in text.chars() {
+        if char == '\r' {
+            continue;
+        }
+
+        if char == '\n' {
+            for _ in 0..3 {
+                push_char_to_stack(chars, ' ', &mut value, &mut counter, &mut writer);
+            }
+            continue;
+        }
+
+        if char.is_numeric() {
+            current_number.push(char.to_string());
             continue;
         } else if current_number.len() > 0 {
-            let number = current_number.join("").parse::<u32>().unwrap();
-        
-            let roman = Roman::from(number).to_string().to_ascii_lowercase();
-
-            for r in roman.chars() {
-                if let Some(abc) = push_char_to_stack(chars, r, &mut value, &mut counter) {
-                    writer.write(abc.to_be_bytes().as_slice()).unwrap();
-                }
+            push_char_to_stack(chars, '1', &mut value, &mut counter, &mut writer);
+            for index in &current_number {
+                let abc: Vec<char> = index_to_char(chars, index.parse::<usize>().unwrap()).chars().collect();
+                push_char_to_stack(chars, abc[0], &mut value, &mut counter, &mut writer);
             }
+            push_char_to_stack(chars, '1', &mut value, &mut counter, &mut writer);
+
             current_number.clear();
         }
-        */
-        //println!("{}", counter);
-        if let Some(value) = push_char_to_stack(chars, t, &mut value, &mut counter) {
-            writer.write(value.to_be_bytes().as_slice()).unwrap();
-        }
-    }
-    /*
-    if current_number.len() > 0 {
-        let number = current_number.join("").parse::<u32>().unwrap();
-        
-        let roman = Roman::from(number).to_string().to_ascii_lowercase();
 
-        for r in roman.chars() {
-            if let Some(abc) = push_char_to_stack(chars, r, &mut value, &mut counter) {
-                writer.write(abc.to_be_bytes().as_slice()).unwrap();
-            }
+        if char.is_uppercase() {
+            push_char_to_stack(chars, '2', &mut value, &mut counter, &mut writer);
+            char = char.to_ascii_lowercase();
         }
+
+        push_char_to_stack(chars, char, &mut value, &mut counter, &mut writer);
     }
-    */
+
+    if current_number.len() > 0 {
+        push_char_to_stack(chars, '1', &mut value, &mut counter, &mut writer);
+        for index in &current_number {
+            let abc: Vec<char> = index_to_char(chars, index.parse::<usize>().unwrap()).chars().collect();
+            push_char_to_stack(chars, abc[0], &mut value, &mut counter, &mut writer);
+        }
+        push_char_to_stack(chars, '1', &mut value, &mut counter, &mut writer);
+    }
 
     if counter > 0 {
         writer.write(value.to_be_bytes().as_slice()).unwrap();
     }
     
-    return writer.buffer().to_vec();
+    writer.flush().unwrap();
+
+    return writer.get_ref().to_vec();
+    //return writer.buffer().to_vec();
 }
 
-fn push_char_to_stack(chars: &str, char: char, value: &mut u128, counter: &mut u8) -> Option<u128> {
-    let index = char_to_index(chars, char);
-    if index.is_none() {
-        return None;
-    }
-    let index = index.unwrap();
+fn push_char_to_stack(chars: &str, char: char, value: &mut u128, counter: &mut u8, writer: &mut BufWriter<Vec<u8>>) {
+    let index = char_to_index(chars, char).unwrap_or(0);
 
-    //println!("{}", counter);
     *value |= (index as u128 & 31) << *counter * 5;
 
     *counter += 1;
     if *counter > 24 {
-        let return_value = value.clone();
+        writer.write(value.to_be_bytes().as_slice()).unwrap();
         *counter = 0;
         *value = 0;
-        return Some(return_value);
     }
-
-    return None;
 }
 
 fn decompress(path: &str, chars: &str) -> String {
     let mut buffer = File::open(path).unwrap();
     
-    let mut string = String::new();
+    let mut is_number = false;
+    let mut next_capitalized = false;
+    let mut space_counter = 0;
+
+    let mut array: Vec<String> = vec![];
+
     loop {
-        //println!("yo");
         let next = buffer.read_u128::<BigEndian>();
         if next.is_err() {
-            //println!("{:#?}", next);
             break;
         }
         let next = next.unwrap();
 
-        //let length = 5;
-        //println!("{}", length);
+        //println!("{:?}", next);
 
-        let mut array = vec![];
         for index in 0..25 {
             let letter = (next >> (index * 5)) & 31;
-            //println!("{}", letter);
-            array.push(index_to_char(chars, letter as usize));
-        }
-        string += &array.join("");
 
-        
+            let char = index_to_char(chars, letter as usize);
+            
+            if char == " " && !is_number {
+                space_counter += 1;
+                
+                continue;
+            } else {
+                if space_counter >= 3 {
+                    while space_counter >= 3 {
+                        array.push("\n".to_owned());
+                        space_counter -= 3;
+                    }
+                    space_counter = 0;
+                }
 
-        /*
-        for index in 0..26 {
-            let letter = (next >> (index*5)) & 31;
-            string += index_to_char(chars, letter as usize);
+                if space_counter > 0 {
+                    array.push(" ".to_owned());
+                    space_counter = 0;
+                }
+            }
+
+            if char == "1" {
+                is_number = !is_number;
+                continue;
+            }
+            if char == "2" {
+                next_capitalized = true;
+                continue;
+            }
+
+            if is_number {
+                let abc: Vec<char> = char.chars().collect();
+                let index = char_to_index(chars, abc[0]).unwrap();
+                array.push(index.to_string())
+            } else {
+                if next_capitalized {
+                    array.push(char.to_ascii_uppercase().to_string());
+                    next_capitalized = false;
+                }else {
+                    array.push(char.to_string());
+                }
+            }
         }
-        */
     }
-    
-    return string;
+
+    return array.join("");
 }
 
 fn index_to_char(chars: &str, index: usize) -> &str {
